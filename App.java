64b21a388968fbe0878fd058b28c824964e47ba5
RuthJimenez309 <79241546+RuthJimenez309@users.noreply.github.com>
2021-04-28 23:54:57 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,10 +1,19 @@
 package Aplication;
 
+import Gestion.Gestion_CVS;
+
 public class App {
     public static void main(String [] args  ){
 
-        /*Se realizara el llamado de la class CSV */
+        Gestion_CVS Read = read ();
+        Read.Gestion_Cvs();
 
+
+    }
+
+    private static Gestion_CVS read() {
+        Gestion_CVS read = read();
+    return  read;
     }
 
 
